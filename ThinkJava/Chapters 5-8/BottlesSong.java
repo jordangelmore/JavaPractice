83328ed67f1203b194fd2268ea419f317f29747f
Jordan Elmore <38922378+jordanelmore-civitas@users.noreply.github.com>
2019-02-19 14:26:14 -0600

    Create BottlesSong.java

---
+++ ThinkJava/Chapters 5-8/BottlesSong.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class BottlesSong {
+
+    public static void songStarter(int n) {
+        int x = n - 1;
+        if (n > 1 ) {
+            System.out.println(n + " bottles of beer on the wall, ");
+            System.out.println(n + " bottles of beer, ");
+            System.out.println("ya’ take one down, ya’ pass it around, ");
+            System.out.println(x +" bottles of beer on the wall." );
+            songStarter(n - 1);
+        } else if (n == 1) {
+            System.out.println(n + " bottle of beer on the wall, ");
+            System.out.println(n + " bottles of beer ");
+            System.out.println("ya’ take one down, ya’ pass it around,");
+            System.out.println("No bottles of beer on the wall, ");
+            songStarter(n - 1);
+        } else {
+            System.out.println("No bottles of beer on the wall, ");
+            System.out.println("no bottles of beer, ");
+            System.out.println("ya’ can’t take one down, ya’ can’t pass it around, ");
+            System.out.println("’cause there are no more bottles of beer on the wall!");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter a number to start the song: ");
+        int n = keyboard.nextInt();
+        songStarter(n);
+
+    }
+
+}
