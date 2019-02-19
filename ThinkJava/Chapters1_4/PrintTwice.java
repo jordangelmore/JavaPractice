4ee863a6c520578cd69b091e280822fc2c7d3af3
Jordan Elmore <38922378+jordanelmore-civitas@users.noreply.github.com>
2019-02-19 09:44:24 -0600

    Create PrintTwice.java

---
+++ ThinkJava/Chapters1_4/PrintTwice.java
@@ -0,0 +1,16 @@
+public class PrintTwice {
+
+    public static void printTwice(String s) {
+        System.out.println(s);
+        System.out.println(s);
+
+    }
+
+    public static void main(String[] args){
+
+        String argument = "Don't make me say this twice!";
+        printTwice(argument);
+
+
+    }
+}
