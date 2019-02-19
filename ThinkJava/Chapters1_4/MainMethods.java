c6d6947b9ff1e74a9f1133936a9a49685dee5b44
Jordan Elmore <38922378+jordanelmore-civitas@users.noreply.github.com>
2019-02-19 09:46:24 -0600

    Create MainMethods.java

---
+++ ThinkJava/Chapters1_4/MainMethods.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MathMethods {
+
+    public static void newLine() {
+
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+
+        double root = Math.sqrt(17.0);
+        double angle = 1.5;
+        double height = Math.sin(angle);
+        double degrees = 90;
+        double radius = (degrees / 180.0) * Math.PI;
+        double radius2 = Math.toRadians(180.0);
+        double degrees2 = Math.toDegrees(Math.PI);
+        long x = Math.round(Math.PI * 20.0);
+        double y = Math.pow(2.0,10.0);
+        newLine();
+
+
+    }
+
+}
